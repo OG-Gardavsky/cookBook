5704199f269348481ebce44ab68392cae4378148
OG-Gardavsky <ogardavsky@gmail.com>
2022-03-22 18:43:44 +0100

    added Boostrap and font awesome dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faBowlRice } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faBowlRice)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(library)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="home">
+    <TopBar />
     <h1>Homepage</h1>
+    <b-button>Button</b-button>
+    <p>filters</p>
+
   </div>
 </template>
 
 <script>
+import TopBar from "@/components/Topbar";
 export default {
   name: 'Home',
   components: {
+    TopBar
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  async created() {
 
   }
 }
