4b6fb53ed4b4925f67b78197d38537490b626fba
OG-Gardavsky <ogardavsky@gmail.com>
2022-03-22 13:46:34 +0100

    vue project init

---
+++ client/vue.config.js
@@ -0,0 +1,12 @@
+// proxy for dev purpose
+module.exports = {
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:1337',
+                changeOrigin: true,
+                logLevel: 'debug',
+            },
+        },
+    },
+}
