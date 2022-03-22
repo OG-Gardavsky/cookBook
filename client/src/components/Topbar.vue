7db03124d687ff1ba7df3fc70ad82e5c5fcd548e
OG-Gardavsky <ogardavsky@gmail.com>
2022-03-22 18:58:44 +0100

    basic topbar created

---
+++ client/src/components/Topbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-navbar class="navBar" toggleable="lg" type="dark">
+      <b-navbar-brand href="#">
+        <font-awesome-icon icon="fa-solid fa-utensils" />
+        CookBook
+      </b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopBar"
+}
+</script>
+
+<style scoped>
+  .navBar {
+    background-color: #f6ac30;
+  }
+
+  .navBar > * {
+    margin-left: 20px;
+  }
+</style>
