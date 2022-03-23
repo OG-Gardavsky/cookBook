3a1a2c1d0c98bba83f466f7d82880c7def716044
OG-Gardavsky <ogardavsky@gmail.com>
2022-03-23 18:14:21 +0100

    dynamic image loading poc

--- client/src/components/RecipeRow.vue
+++ client/src/components/RecipeRow.vue
@@ -3,7 +3,8 @@
     class="recipe container d-flex flex-row border-bottom justify-content-between col-sm text-decoration-none"
     :to="`/recipeDetail?id=${recipe.id}`">
 
-    <img class="img-fluid w-25" :src="'http://localhost:1337/uploads/full_v_recept733_b3d8cb05d7.jpg'"/>
+<!--    need to be rewrited for final implementation-->
+    <img class="img-fluid w-25" :src="`http://localhost:1337${this.photoUrl}`"/>
     <div>{{recipe.attributes.title}} </div>
     <div>{{recipe.attributes.difficulty}}</div>
     <div>{{recipe.attributes.timeToPrepare}} min.</div>
@@ -17,6 +18,19 @@ export default {
   name: "RecipeRow",
   props: {
     recipe: Object,
+  },
+  data() {
+    return {
+      photoUrl: null
+    }
+  },
+  created() {
+    try {
+      this.photoUrl = this.recipe.attributes.photo.data[0].attributes.url
+    } catch {
+      // this.photoUrl = 'default'
+    }
+
   }
 }
 </script>
